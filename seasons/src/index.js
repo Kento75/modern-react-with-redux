88587bcce379bb2ae861f301afc1daa42f3db798
Kento75 <kento2github@gmail.com>
2019-03-03 07:59:32 +0900

    Add legacy lifecycle componentDidMount

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,15 +6,12 @@ class App extends React.Component {
     super(props);
 
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // we called setstate
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message })
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
