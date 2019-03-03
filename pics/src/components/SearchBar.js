0b281ada1c027be814b260bbd7fa50f4b915ff8c
Kento75 <kento2github@gmail.com>
2019-03-03 20:02:37 +0900

    Add mock eventHandler

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+
+  onInputChange(event) {
+    console.log(event.target.value)
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" onChange={ this.onInputChange } />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
