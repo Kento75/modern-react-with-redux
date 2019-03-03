4642caf33084dfb3114dfb069782e49e5620914d
Kento75 <kento2github@gmail.com>
2019-03-04 06:21:40 +0900

    Add delivery state less functions

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
 
-  onInputChange(event) {
-    console.log(event.target.value)
+  state = { term: "" }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    
+    this.props.onSubmit(this.state.term)
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={ this.onInputChange } />
+            <input type="text" value={this.state.term} onChange={ (e) => this.setState({term: e.target.value}) } />
           </div>
         </form>
       </div>
