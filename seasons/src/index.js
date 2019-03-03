f94a4dcdabc2d2703057dc540f592ba8a7c4ab1a
Kento75 <kento2github@gmail.com>
2019-03-03 14:41:46 +0900

    Add default props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     if(!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat} />
     }
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
