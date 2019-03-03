21b581a07c1910fbb57482a4ca28b65f564b67b3
Kento75 <kento2github@gmail.com>
2019-03-03 14:58:43 +0900

    Add renderContent function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if(this.state.errorMessage && !this.state.lat) {
       return(
       <div>
@@ -25,6 +25,14 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
