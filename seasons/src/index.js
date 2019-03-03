b9f5ff0129bb53d29b8414e72981c7ca5aaf6ede
Kento75 <kento2github@gmail.com>
2019-03-03 09:03:32 +0900

    Add Spinner component and Display layout

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMessage: "" };
-  }
+  state = { lat: null, errorMessage: "" }
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -24,13 +22,9 @@ class App extends React.Component {
     }
 
     if(!this.state.errorMessage && this.state.lat) {
-      return (
-        <div>
-          Latitude: {this.state.lat}
-        </div>
-      );
+      return <SeasonDisplay lat={this.state.lat} />
     }
-    return <div>Loading!</div>;
+    return <Spinner />;
   }
 }
 
