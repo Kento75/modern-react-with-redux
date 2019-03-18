9ef5bdaf751a72443c0cb7c15f3091612414dcd0
Kento75 <kento2github@gmail.com>
2019-03-19 06:26:41 +0900

    Add youtube api and get action

---
+++ videos/src/api/youtube.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+import {
+  API_KEY
+} from "../config";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: API_KEY,
+  }
+});
---
+++ videos/src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
+
+class App extends React.Component {
+  state = {videos: []};
+  onTermSubmit = async term => {
+    const response = await youtube.get ('/search', {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState ({videos: response.data.items});
+  };
+
+  render () {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} videos.
+      </div>
+    );
+  }
+}
+
+export default App;
