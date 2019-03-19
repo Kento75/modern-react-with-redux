ac24c6c0277a1593eb094a071df5c34a79c8b798
Kento75 <kento2github@gmail.com>
2019-03-19 21:24:31 +0900

    Add videoItem compoennt

--- videos/src/components/App.jsx
+++ videos/src/components/App.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 
+import VideoList from '../components/VideoList';
+
 class App extends React.Component {
   state = {videos: []};
+
   onTermSubmit = async term => {
     const response = await youtube.get ('/search', {
       params: {
@@ -19,7 +22,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        I have {this.state.videos.length} videos.
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
