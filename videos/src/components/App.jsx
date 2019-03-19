a061dcd8bfafe3698d7a76091a05f22b425bca0e
Kento75 <kento2github@gmail.com>
2019-03-20 06:28:41 +0900

    Add video player

--- videos/src/components/App.jsx
+++ videos/src/components/App.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 
-import VideoList from '../components/VideoList';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-  state = {videos: []};
+  state = {videos: [], selectedVideo: null};
 
   onTermSubmit = async term => {
     const response = await youtube.get ('/search', {
@@ -18,11 +19,27 @@ class App extends React.Component {
     this.setState ({videos: response.data.items});
   };
 
+  onVideoSelect = video => {
+    this.setState ({selectedVideo: video});
+  };
+
   render () {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- videos/src/components/VideoItem.jsx
+++ videos/src/components/VideoItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const VideoItem = ({video}) => {
+const VideoItem = ({video, onVideoSelect}) => {
   return (
-    <div className="item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+    <div onClick={() => onVideoSelect (video)} className="item">
+      <img
+        className="ui image"
+        alt={video.snippet.title}
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
-      {video.snippet.title}
     </div>
   );
 };
