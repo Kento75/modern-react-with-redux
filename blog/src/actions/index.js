e64c964d3a2fab73843dfc2f681ee6dcc07a053d
Kento75 <kento2github@gmail.com>
2019-03-24 08:18:12 +0900

    Add memoized fetch

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash"
 import jsonPlaceholder from "../apis/jsonPlaceholder"
 
 export const fetchPosts = () => async (dispatch) => {
@@ -8,10 +9,12 @@ export const fetchPosts = () => async (dispatch) => {
   })
 }
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = (id) => async dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`)
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   })
-}
+})
