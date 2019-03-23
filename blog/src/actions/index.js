61e346bc04d0ea598eb431d37d44fa065ecc1996
Kento75 <kento2github@gmail.com>
2019-03-24 08:38:30 +0900

    Add Action Creaters in Action Creaters

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,18 @@
 import _ from "lodash"
 import jsonPlaceholder from "../apis/jsonPlaceholder"
 
-export const fetchPosts = () => async (dispatch) => {
+// Action Creaters in Action Creaters パターン
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  // ポスト一覧取得
+  await dispatch(fetchPosts())
+
+  // ユーザーID重複排除
+  const userIds = _.uniq(_.map(getState().posts, "userId"))
+  // ユーザー名の取得
+  userIds.forEach(id => dispatch(fetchUser(id)))
+}
+
+export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts")
   dispatch({
     type: "FETCH_POSTS",
@@ -9,12 +20,21 @@ export const fetchPosts = () => async (dispatch) => {
   })
 }
 
-export const fetchUser = (id) => async dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`)
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   })
-})
+}
+
+// memoizeパターン
+// export const fetchUser = (id) => async dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`)
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data
+//   })
+// })
