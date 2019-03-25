9be960ffa604e1d3f202779c70417e5cd30f22e0
Kento75 <kento2github@gmail.com>
2019-03-26 06:28:28 +0900

    Add scaffoliding component

--- streams/client/src/components/App.jsx
+++ streams/client/src/components/App.jsx
@@ -1,30 +1,20 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-
-const PageOne = () => {
-  return (
-    <div>
-      Page One
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      Page Two
-      <Link to="/">Navigate to Page One</Link>;
-    </div>
-  );
-};
+import StreamsCreate from './streams/StreamCreate';
+import StreamsEdit from './streams/StreamEdit';
+import StreamsDelete from './streams/StreamDelete';
+import StreamsList from './streams/StreamList';
+import StreamsShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" exact component={PageTwo} />
+        <Route path="/" exact component={StreamsList} />
+        <Route path="/streams/new" exact component={StreamsCreate} />
+        <Route path="/streams/edit" exact component={StreamsEdit} />
+        <Route path="/streams/delete" exact component={StreamsDelete} />
+        <Route path="/streams/show" exact component={StreamsShow} />
       </BrowserRouter>
     </div>
   );
