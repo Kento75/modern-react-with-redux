dd5414f7bf9ed14b9a6e13b18c4c878a0ef6e862
Kento75 <kento2github@gmail.com>
2019-03-30 10:06:36 +0900

    Add reduxfrom

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,8 +1,9 @@
-import {
-  combineReducers
-} from "redux";
-import authReducer from "./authReducer";
+import {combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form';
 
-export default combineReducers({
-  auth: authReducer
+import authReducer from './authReducer';
+
+export default combineReducers ({
+  auth: authReducer,
+  form: formReducer,
 });
