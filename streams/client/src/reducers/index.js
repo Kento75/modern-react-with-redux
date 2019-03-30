609b6bf8772162396f1378190f6b94f46929e8b8
Kento75 <kento2github@gmail.com>
2019-03-31 07:50:11 +0900

    Add getStreams logic and routing component

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,9 +1,15 @@
-import {combineReducers} from 'redux';
-import {reducer as formReducer} from 'redux-form';
+import {
+  combineReducers
+} from 'redux';
+import {
+  reducer as formReducer
+} from 'redux-form';
 
 import authReducer from './authReducer';
+import streamReducer from "./streamReducer"
 
-export default combineReducers ({
+export default combineReducers({
   auth: authReducer,
   form: formReducer,
+  streams: streamReducer
 });
