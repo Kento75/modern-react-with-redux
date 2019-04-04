c21eefce767e84b0c84f663dfbf07cb611e921b0
Kento75 <kento2github@gmail.com>
2019-04-05 06:10:01 +0900

    Fix video mount logic

--- streams/client/src/components/streams/StreamShow.jsx
+++ streams/client/src/components/streams/StreamShow.jsx
@@ -13,14 +13,33 @@ class StreamShow extends React.Component {
 
   componentDidMount () {
     const {id} = this.props.match.params;
+
     this.props.fetchStream (id);
+    this.buildPlayer ();
+  }
+
+  componentDidUpdate () {
+    this.buildPlayer ();
+  }
+
+  componentWillUnmount () {
+    this.player.destroy ();
+  }
+
+  buildPlayer () {
+    // すでにビデオを読み込んでいた場合
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const {id} = this.props.match.params;
     this.player = flv.createPlayer ({
       type: 'flv',
-      url: `http://localhost:8000/live/${id}`,
+      url: `http://localhost:8000/live/${id}.flv`,
     });
     this.player.attachMediaElement (this.videoRef.current);
     this.player.load ();
   }
+
   render () {
     if (!this.props.stream) {
       return <div>Loading...</div>;
