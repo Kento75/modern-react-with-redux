dfbab2c29117f5f9c174926572d62d2e2decd84f
Kento75 <kento2github@gmail.com>
2019-04-06 13:29:00 +0900

    Add context component

--- translate/src/components/App.jsx
+++ translate/src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
   state = {language: 'english'};
@@ -23,7 +25,11 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange ('dutch')}
           />
         </div>
-        <UserCreate />
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
